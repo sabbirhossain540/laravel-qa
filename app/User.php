a043f8196a070e961a1e1f0def25a2f9a421d2b7
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2018-11-20 04:14:31 -0800

    generate Question model via php artisan

--- app/User.php
+++ app/User.php
@@ -27,4 +27,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function questions(){
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
