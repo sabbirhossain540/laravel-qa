f75f26a400cac331d0add8832bcb06274e4040ce
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2018-11-20 04:38:09 -0800

    Create Question Factory and Migration

--- app/Question.php
+++ app/Question.php
@@ -10,4 +10,9 @@ class Question extends Model
     public function user(){
     	return $this->belongsTo(User::class);
     }
+
+    public function setTitleAttribute($value){
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -32,8 +32,5 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-    public function setTitleAttribute($value){
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);
-    }
+    
 }
