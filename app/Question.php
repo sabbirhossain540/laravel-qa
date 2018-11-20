353a6bcaef44604f8239a66edaf49cc1f7536319
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2018-11-20 06:59:03 -0800

    Stating Work with Vue js

--- app/Question.php
+++ app/Question.php
@@ -23,4 +23,17 @@ class Question extends Model
     public function getCreatedDateattribute(){
     	return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute(){
+    	if($this->answers > 0){
+    		if($this->best_answer_id){
+    			return "answered-accepted";
+    		}
+
+    		return "answered";
+    	}
+
+    	return "unanswered";
+
+    }
 }
